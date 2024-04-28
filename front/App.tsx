7702fb65cd959fda6491cca0a2081a3626196460
김명식 <rlaaudtlr233@naver.com>
2024-04-28 21:05:37 +0900

    stack navigation 타입핑

--- front/App.tsx
+++ front/App.tsx
@@ -4,35 +4,11 @@ import React from 'react';
 import AuthStackNavigator from './src/navigation/AuthStackNavigator';
 
 function App(): JSX.Element {
-  // const [name, setName] = useState('');
-  // const handleChangeInput = (text: string) => {
-  //   console.log(text);
-  //   setName(text);
-  // };
-
   return (
     <NavigationContainer>
       <AuthStackNavigator />
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   input: {
-//     flex: 1,
-//     borderWidth: 2,
-//     borderColor: 'black',
-//     height: 50,
-//     width: 100,
-//   },
-//   inputContainer: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//   },
-// });
 
 export default App;
--- front/src/screens/AuthHomeScreen.tsx
+++ front/src/screens/AuthHomeScreen.tsx
@@ -1,13 +1,21 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Button, SafeAreaView, View} from 'react-native';
+import {AuthStackParamList} from '../navigation/AuthStackNavigator';
+import {authNavigations} from '../constants';
 
-export default function AuthHomeScreen({navigation}) {
+type Props = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
+
+export default function AuthHomeScreen({navigation}: Props) {
   return (
     <SafeAreaView>
       <View>
         <Button
           title="로그인 화면으로 이동"
-          onPress={() => navigation.navigate('Login')}
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
       </View>
     </SafeAreaView>
